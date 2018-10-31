5608292bbd1c87ecb9824a644fe49c302416e37a
Ram Rana <ramrana1912@gmail.com>
2018-10-31 22:35:06 +0530

    models table updation

--- xversion/booking/models.py
+++ xversion/booking/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 
 
-class Image(models.Model):
+class Image(models.Model):   #categoryImages
     img = models.ImageField(upload_to="bike/category/", null=True, blank=True)
 
     class Meta:
@@ -15,7 +15,7 @@ class Image(models.Model):
         return str(self.img)
 
 
-class Category(models.Model):
+class Category(models.Model):   #Categorytable
         c_id = models.AutoField(primary_key=True)
         category_name = models.CharField(max_length=60)
         image = models.ForeignKey(Image, db_column='image', null=True, on_delete=models.CASCADE)
@@ -33,6 +33,46 @@ class Category(models.Model):
 
         def get_absolute_url(self):
             return reverse('booking:category_list', args=[self.c_id, self.slug])
+
+
+class Model_Image(models.Model):                  #for storage of Models
+    img = models.ImageField(upload_to="bike/category/", null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Model Images'
+        db_table = 'Model_Image'
+
+    def __str__(self):
+        return str(self.img)
+
+
+
+class Category_Model(models.Model):   #Model storage table
+    m_id = models.AutoField(primary_key=True)
+    c_id=models.ForeignKey(Category,null=True,on_delete=models.CASCADE)
+    #dealer_id=models.ForeignKey()
+    model_name=models.CharField(max_length=100)
+    price=models.IntegerField()
+    model_image = models.ForeignKey(Model_Image, db_column='image', null=True, on_delete=models.CASCADE)
+    rating=models.IntegerField()
+    status=models.BooleanField(default=False)
+    specification=models.CharField(max_length=300)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name_plural ='Models/Variants'
+        db_table = 'Category_Model'
+
+    def __str__(self):
+        return self.model_name
+
+
+
+
+
 
 
 
