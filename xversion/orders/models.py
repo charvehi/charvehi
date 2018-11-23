9059a828b3308b484e3d05b015943bd7dcb9b501
radioactive <ayush4920@gmail.com>
2018-11-23 17:13:46 +0530

    signin and map working. no bg and logo image.

---
+++ xversion/orders/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class UserOrderInfo(models.Model):
+    uid = models.BigIntegerField(primary_key=True, default=0)
+    mid = models.IntegerField()
+    lat = models.DecimalField(max_digits=9, decimal_places=6, blank=True, default=0)
+    lon = models.DecimalField(max_digits=9, decimal_places=6, blank=True, default=0)
+    start = models.DateTimeField(auto_now_add=True)
+    end = models.DateTimeField(auto_now=True)
+
+    #REQUIRED_FIELDS = ['email', 'password']
+
+    class Meta:
+        verbose_name_plural = 'Orders'
+        db_table = 'orders'
