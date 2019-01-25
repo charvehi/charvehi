3a9c78c039218abc24e22705c7aaf1136f40d8bb
Ram Rana <ramrana1912@gmail.com>
2019-01-25 23:47:50 +0530

    main urls

--- xversion/xversion/urls.py
+++ xversion/xversion/urls.py
@@ -17,23 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
-from uaccounts import views as acc
+
 from booking import views as book
 from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-
+from UserAccounts.forms import NewPasswordResetForm
+from django.contrib.auth.views import (LoginView,LogoutView,PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('booking/', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^$', book.index, name='index'),
-    url(r'^$', acc.index, name='index'),
-    url(r'^special/', acc.special,name='special'),
-    url(r'^uaccounts/', include('uaccounts.urls')),
+    url(r'^dealer/',include('dealer.urls',namespace='dealer')),#url for dealr rr
+    url(r'^accounts/',include('UserAccounts.urls',namespace='useraccounts')),#replace uaccounts later above url
     url(r'^bike/', include('booking.urls'), name='category'),
     url(r'^bike/book/', include('orders.urls'), name='orders'),
     url(r'^bike/reviews/', include('review.urls', namespace="review")),
-    url(r'^signout/$', acc.user_logout, name='logout'),
+
+    url(r'^password_reset/$', PasswordResetView.as_view(form_class=NewPasswordResetForm,template_name='registration/password_reset_form.html'), name='password_reset'),
+    url(r'^password_reset/done/$', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    url(r'^reset/done/$',PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html') , name='password_reset_complete'),
+
     #path('', include('booking.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
