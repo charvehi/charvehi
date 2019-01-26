5fd795c7cb178d97bb823e9af4c648e14adcaee6
charvehi <44134702+charvehi@users.noreply.github.com>
2019-01-26 10:43:30 +0530

    Update settings.py

--- xversion/xversion/settings.py
+++ xversion/xversion/settings.py
@@ -16,8 +16,8 @@ import os
 #gmail or google apps
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ramrana1912@gmail.com'
-EMAIL_HOST_PASSWORD = 'mepassword'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 '''EMAIL_HOST = ''
