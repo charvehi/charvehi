1bb93f3096ace47ab9b209fb3f47cdccc503fc03
radioactive <ayush4920@gmail.com>
2019-01-31 21:57:02 +0530

    forms for index

--- xversion/booking/forms.py
+++ xversion/booking/forms.py
@@ -7,8 +7,11 @@ class IndexBookForm(forms.ModelForm):
     #lat = forms.DecimalField(max_digits=9, decimal_places=6)
     #lon = forms.DecimalField(max_digits=9, decimal_places=6)
     location = forms.CharField(widget=forms.TextInput(attrs={'id': 'loc'}), max_length=200)
-    start_time = forms.CharField(widget=forms.TextInput(attrs={'class': 'time'}), max_length=50)
-    end_time = forms.CharField(widget=forms.TextInput(attrs={'class': 'time'}), max_length=50)
+    start_date = forms.DateField(widget=forms.TextInput(attrs={'class': 'date start', 'id': 'startDate',
+                                                               'aria-hidden': 'true'}))
+    start_time = forms.CharField(widget=forms.TextInput(attrs={'class': 'time start', 'id': 'startTime'}), max_length=50)
+    end_date = forms.DateField(widget=forms.TextInput(attrs={'class': 'date end', 'id': 'endDate'}))
+    end_time = forms.CharField(widget=forms.TextInput(attrs={'class': 'time end', 'id': 'endTime'}), max_length=50)
 
     REQUIRED_FIELDS = ['location', 'start_date', 'start_time', 'end_date', 'end_time']
 
